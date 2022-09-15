46a66bd507fe2b7c8b6045addc02eb9f7fbf7fa2
ArmDoc <zarman32@yandex.ru>
2022-09-15 20:20:39 +0400

    refactoring

--- database/seeders/CurrenciesTableSeeder.php
+++ database/seeders/CurrenciesTableSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Currency;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
+/** @package Database\Seeders */
 class CurrenciesTableSeeder extends Seeder
 {
 	/**
@@ -14,8 +17,9 @@ class CurrenciesTableSeeder extends Seeder
 	 */
 	public function run()
 	{
-		\App\Models\Currency::factory()->create([
-			'name' => 'Test money',
-		]);
+		$currencies = ['USD', 'EUR', 'RUB'];
+		foreach ($currencies as $currency) {
+			Currency::create(['name' => $currency]);
+		}
 	}
 }
