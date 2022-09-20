9d047196abd9c3300d19ad708effca2f41ce062b
ArmDoc <zarman32@yandex.ru>
2022-09-20 12:40:21 +0400

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,25 +9,26 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-	/**
-	 * Define the application's command schedule.
-	 *
-	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-	 * @return void
-	 */
-	protected function schedule(Schedule $schedule)
-	{
-		$schedule->command('currency:update')->dailyAt('02:00');
-	}
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+		//  $schedule->command('currency:update')->dailyAt('02:00');
+		 $schedule->command('currency:update')->everyThreeMinutes();
+    }
 
-	/**
-	 * Register the commands for the application.
-	 *
-	 * @return void
-	 */
-	protected function commands()
-	{
-		$this->load(__DIR__ . '/Commands');
-		require base_path('routes/console.php');
-	}
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+        require base_path('routes/console.php');
+    }
 }
